44cfbfe807d6d2a97296597eeea2509c1a5b353f
mani53-dev <manikhan5353@gmail.com>
2025-02-07 13:32:53 +0500

    Updated main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
 
   runApp(
     GetMaterialApp(
-      title: 'Bossie',
+      title: 'GetX',
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
       debugShowCheckedModeBanner: false,
